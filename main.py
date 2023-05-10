a832828c601b55d85b1034e8a6da13cb85f203f5
Lijian Wang <lijiwang@linkedin.com>
2023-05-09 22:14:04 -0700

    meh

---
+++ main.py
@@ -0,0 +1,42 @@
+import time
+import subprocess
+
+COMMAND_FILE = 'command.txt'
+HISTORY_FILE = 'history.txt'
+SETUPENV = "source ~/.bashrc && "
+
+def compute_line():
+    with open(HISTORY_FILE) as f:
+        lines = f.readlines()
+    lines = list(filter(lambda x: x.strip() != "", lines))
+    return len(lines)
+
+def read_command(n):
+    with open(COMMAND_FILE) as f:
+        lines = f.readlines()
+        if n >= len(lines):
+            return None
+        return lines[n]
+
+def append_hist():
+    with open(HISTORY_FILE, 'a') as f:
+        f.write(f"{time.time()}\n")
+
+def run():
+    '''
+    return number of seconds to sleep
+    '''
+    n = compute_line()
+    cmd = read_command(n)
+    if cmd is None:
+        print("cmd none. end of file")
+        return 0
+    cmd = SETUPENV + cmd
+    p = subprocess.Popen(cmd, shell=True)
+    p.wait()
+    append_hist()
+    return 0
+
+while 1:
+    c = run()
+    time.sleep(c)
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 chains the execution of multiple commands
 
 # how to use it? 
+add the command you want to run in command.txt
+if you are using VScode with autosave on, try to append command as a whole directly rather than edit in the VScode window so as to prevent the script from reading in partial command
